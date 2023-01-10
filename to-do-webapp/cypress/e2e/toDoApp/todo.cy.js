8aab13b8b1265199fedfbb28cfbdc15133355a0f
Hazar Armiti <hazara@PRO.COM>
2023-01-10 13:53:05 +0200

    prepare cypress env

---
+++ to-do-webapp/cypress/e2e/toDoApp/todo.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('to-do app', () => {
+  // beforeEach(() => {
+  //   cy.visit('https://example.cypress.io/todo')
+  // })
+
+  it('check the title of the app', () => {
+
+  });
+});
